a614e422b16efdc2c3c2ebe10a646084b0928fae
Àlex Castelo Martínez <alexcastelomartinez@gmail.com>
2023-09-03 11:21:26 +0200

    Fix CI

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/fe-mentor-api-country-list/",
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
